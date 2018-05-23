dcae31087988b73a83942f3e4869c49494151627
Gregory Kysenko <heyapple1395@gmail.com>
2018-05-24 01:36:24 +0300

    Implemented iss view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import Location from './components/Location';
 
 class App extends Component {
   constructor() {
@@ -14,15 +15,22 @@ class App extends Component {
   componentDidMount(){
     this.interval = setInterval(() => axios.get('http://api.open-notify.org/iss-now.json')
     .then(e => this.setState({cosmicInfo: e}))
-    ,10000);
+    ,5000);
   }
+  componentWillMount(){
+    axios.get('http://api.open-notify.org/iss-now.json')
+    .then(e => this.setState({cosmicInfo: e}));
+
+  }
+
   componentWillUnmount() {
    clearInterval(this.interval);
   }
   render() {
-    console.log(this.state.cosmicInfo);
     return (
       <div className="App">
+        <div className="coordsWrap">
+        <Location coords={this.state.cosmicInfo.data}/></div>
 
       </div>
     );
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 
-class App extends Component {
+class Location extends Component {
   render() {
+
+    if (this.props.coords) {
+      let position = this.props.coords.iss_position;
+      console.log(position);
+      return (
+        <div className="Location">
+          <h4>ISS current position:</h4> 
+          <h5> latitude: {position.latitude} longitude: {position.longitude} </h5>
+        </div>
+      );
+    }
     return (
-      <div className="App">
+      <div className="Location">
 
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default Location;
